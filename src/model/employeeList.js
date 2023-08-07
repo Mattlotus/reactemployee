ef63712b478febccb9be0cb02089d16ea6bfefbd
Mattlotus <mattlotus91@gmail.com>
2023-08-07 00:39:12 -0400

    final

--- src/model/employeeList.js
+++ src/model/employeeList.js
@@ -1,57 +1,57 @@
 const personnel = [
     {
-        name: 'James King',
-        title: 'President and CEO',
+        name: 'Miles Morales',
+        title: 'Web-Slinger',
         office: '781-000-002',
         mobile: '617-000-0002',
         sms: '617-000-0002',
-        email: 'jamesKing@fakemail.com',
-        img: 'https://www.citypng.com/public/uploads/preview/free-round-flat-male-portrait-avatar-user-icon-png-11639648873oplfof4loj.png'
+        email: 'morales@fakemail.com',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOAcISXDFTAmWMies0oWmDGAE4hIQC2KiR-_cK1aUgAm6_QRbnGJsaJYD52j9co6U8m6o&usqp=CAU'
     },
     {
-        name: 'Julie Taylor',
-        title: 'VP of Marketing',
+        name: 'Gwen Stacey',
+        title: 'Web-Slinger',
         office: '931-450-028',
         mobile: '617-030-0004',
         sms: '617-030-0004',
-        email: 'jtaylor@fakemail.com',
-        img: 'https://img.freepik.com/free-icon/user_318-219687.jpg'
+        email: 'gwen@fakemail.com',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0BHx8mK6ECjLh_cLjcWDGA8AbpJis1E7nhrL0cdHtFa7-sf6iCB6qV7pFqPlZRVDAHXc&usqp=CAU'
     },
     {
-        name: 'Eugene Lee',
-        title: 'CFO',
+        name: 'Doc Oct',
+        title: 'Scientist',
         office: '681-000-002',
         mobile: '517-010-0006',
         sms: '517-010-0006',
-        email: 'elee@fakemail.com',
-        img: 'https://media.istockphoto.com/id/1094782736/vector/employee-avatar-male.jpg?s=170667a&w=0&k=20&c=zVTkpt0SF9N3wVTsWcTMusMlyKkiD1IjiaEL3FvCpBc='
+        email: 'doc@fakemail.com',
+        img: 'https://www.themarysue.com/wp-content/uploads/2019/04/doc-ock-spider-verse-sequel-excited.jpg?fit=1200%2C650'
     },
     {
-        name: 'John Williams',
-        title: 'VP of Engineering',
+        name: 'Miguel Ohara',
+        title: 'Dimension Traveler',
         office: '221-450-002',
         mobile: '617-250-0012',
         sms: '617-250-0012',
-        email: 'johnwilliams@fakemail.com',
-        img: 'https://cdn.icon-icons.com/icons2/2643/PNG/512/male_boy_person_people_avatar_icon_159358.png'
+        email: 'miguel@fakemail.com',
+        img: 'https://www.looper.com/img/gallery/the-across-the-spider-verse-fan-theory-that-has-fans-second-guessing-miguel/l-intro-1685745826.jpg'
     },
     {
-        name: 'Ray Moore',
-        title: 'VP of Sales',
+        name: 'Spider Woman',
+        title: 'HERO',
         office: '700-000-022',
         mobile: '600-000-0222',
         sms: '600-000-0222',
-        email: 'rmoore@fakemail.com',
-        img: 'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Picture.png'
+        email: 'spidey@fakemail.com',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9Ke95o4QiUu9J9phi7EMQJxs3-0jsVM7snes-nmUjHwm0Er_VaobAnL0ulOO5RiyoRzA&usqp=CAU'
     },
     {
-        name: 'Paul Jones',
-        title: 'QA Manager',
+        name: 'Hobie Brown',
+        title: 'anarchist',
         office: '777-000-003',
         mobile: '666-000-0033',
         sms: '666-000-0033',
-        email: 'pJones@fakemail.com',
-        img: 'https://cdn-icons-png.flaticon.com/512/147/147142.png'
+        email: 'Hobie@fakemail.com',
+        img: 'https://hips.hearstapps.com/hmg-prod/images/across-the-spiderverse-spider-punk-647f249322695.png?crop=0.9954086317722681xw:1xh;center,top&resize=1200:*'
     }
 ]
 
